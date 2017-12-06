f6d0f6106e050f2b17dbfee36e09f2149e297c1b
Allan Porras <alpocr@Allans-iMac.local>
2017-12-06 10:17:00 -0600

    github settings on theme optinos

--- source/conf.py
+++ source/conf.py
@@ -90,7 +90,10 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': '4GeeksDev',
+    'github_repo': '4gp-documentation',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
